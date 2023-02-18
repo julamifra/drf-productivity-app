2d9c1e19f73d7067261c6033d672ccc82821e873
julamifra <julamifra@gmail.com>
2023-02-18 09:18:45 +0000

    add generic views

--- tasks/views.py
+++ tasks/views.py
@@ -1,73 +1,78 @@
 from django.http import Http404
-from rest_framework import status, permissions
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 from .models import Task
 from .serializers import TaskSerializer
 from drf_productivity_app.permissions import IsOwnerOrReadOnly
 
 
-class TasksList(APIView):
+class TasksList(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
+    queryset = Task.objects.all()
 
-    def get(self, request):
-        tasks = Task.objects.all()
-        serializer = TaskSerializer(
-            tasks, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-    def post(self, request):
-        serializer = TaskSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    # def get(self, request):
+    #     tasks = Task.objects.all()
+    #     serializer = TaskSerializer(
+    #         tasks, many=True, context={'request': request}
+    #     )
+    #     return Response(serializer.data)
+
+    # def post(self, request):
+    #     serializer = TaskSerializer(
+    #         data=request.data, context={'request': request}
+    #     )
+    #     if serializer.is_valid():
+    #         serializer.save(owner=request.user)
+    #         return Response(
+    #             serializer.data, status=status.HTTP_201_CREATED
+    #         )
+    #     return Response(
+    #         serializer.errors, status=status.HTTP_400_BAD_REQUEST
+    #     )
 
 
-class TaskDetail(APIView):
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = TaskSerializer
+    queryset = Task.objects.all()
 
-    def get_object(self, pk):
-        try:
-            task = Task.objects.get(pk=pk)
-            self.check_object_permissions(self.request, task)
-            return task
-        except Task.DoesNotExist:
-            raise Http404
+    # def get_object(self, pk):
+    #     try:
+    #         task = Task.objects.get(pk=pk)
+    #         self.check_object_permissions(self.request, task)
+    #         return task
+    #     except Task.DoesNotExist:
+    #         raise Http404
 
-    def get(self, request, pk):
-        task = self.get_object(pk)
-        serializer = TaskSerializer(
-            task, context={'request': request}
-        )
-        return Response(serializer.data)
+    # def get(self, request, pk):
+    #     task = self.get_object(pk)
+    #     serializer = TaskSerializer(
+    #         task, context={'request': request}
+    #     )
+    #     return Response(serializer.data)
 
-    def put(self, request, pk):
-        task = self.get_object(pk)
-        serializer = TaskSerializer(
-            task, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    # def put(self, request, pk):
+    #     task = self.get_object(pk)
+    #     serializer = TaskSerializer(
+    #         task, data=request.data, context={'request': request}
+    #     )
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(
+    #         serializer.errors, status=status.HTTP_400_BAD_REQUEST
+    #     )
 
-    def delete(self, request, pk):
-        task = self.get_object(pk)
-        task.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    # def delete(self, request, pk):
+    #     task = self.get_object(pk)
+    #     task.delete()
+    #     return Response(
+    #         status=status.HTTP_204_NO_CONTENT
+    #     )
